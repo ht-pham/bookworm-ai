210850b013d7b853e2751fc6885478cd28128433
Huong Pham <40278737+ht-pham@users.noreply.github.com>
2023-03-22 00:37:10 -0500

    Update __init__.py for tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 #from flask_migrate import Migrate
 #from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config=True)
 #app.config.from_object(Config)
 #db = SQLAlchemy(app)
 #migrate = Migrate(app, db)
